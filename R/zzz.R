9ea21deadb965730544d72af116e32f6255ccab1
Auburngrads <Auburngrads@live.com>
2021-01-27 17:45:38 -0500

    Update R/zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+#.onLoad <- function(libname, pkgname) {
+#  
+  # .afit_thesis_bib <<- 'inst/rmarkdown/templates/afit_thesis/skeleton/references/my_bib.bib'
+  # .afit_thesis_template <<- system.file('rmarkdown',
+  #                                       'templates',
+  #                                       'afit_thesis',
+  #                                       'resources',
+  #                                       'templateb.tex',
+  #                                       package = 'DODschools')
+  # }
+
+
+.onUnload <- function (libpath) {
+  library.dynam.unload("Ascend", libpath)
+}
