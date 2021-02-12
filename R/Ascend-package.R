2f95b35ed121ce9b8ea1c40e31f561a9f89c7501
Auburngrads <Auburngrads@live.com>
2021-02-12 15:53:00 -0500

    Update R/Ascend-package.R

--- R/Ascend-package.R
+++ R/Ascend-package.R
@@ -1,7 +1,10 @@
 #' Ascend: Document Templates and Functions for Use at Ascend Innovations
 #' 
-#' @useDynLib "Ascend", registration=TRUE
+#' @useDynLib Ascend, .registration = TRUE
 #' @importFrom Rcpp evalCpp
+#' @importFrom stringr str_c
+#' @import knitr 
 #' @docType package
-#' @rdname Ascend-package
-"_PACKAGE"
+#' @description This is some shit
+#' @name Ascend-package
+NULL
